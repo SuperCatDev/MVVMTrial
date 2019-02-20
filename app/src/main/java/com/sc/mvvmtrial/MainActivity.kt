458ebab4da01098d272ceea93fa754fead74c142
NTimchenko <ntimchenko@niisokb.ru>
2019-02-20 15:38:36 +0300

    Created project

---
+++ app/src/main/java/com/sc/mvvmtrial/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sc.mvvmtrial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
